c2b795a518140db1c309e97a0dbec13f46c3e6ed
Иван К. PC <itehnoviking@gmail.com>
2022-01-24 17:13:18 +0300

    Add project files.

---
+++ CoinStore/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using CoinStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoinStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
